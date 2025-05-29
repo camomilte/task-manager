b9d8a8b3767f6a5eae5c8ad8d13b7b7ccde5f06f
Camomilte <camomilte@gmail.com>
2025-05-29 20:29:23 +0200

    add-task-form ui done and some comments added

--- src/app/(root)/@authenticated/(admin)/add-task/_components/add-task-form.jsx
+++ src/app/(root)/@authenticated/(admin)/add-task/_components/add-task-form.jsx
@@ -1,12 +1,18 @@
 "use client"
 
-import { Check, ChevronsUpDown } from "lucide-react"
-
-// Import functions
+// Import external packages
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { parse } from "date-fns";
+import { eachDayOfInterval, parse } from "date-fns";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+
+// Import icons
+import { TbAlertCircleFilled } from "react-icons/tb";
+import { Check, ChevronsUpDown } from "lucide-react"
+
+// Import utility functions
 import { cn } from "@/lib/utils";
 
 // Import ui components
@@ -15,13 +21,15 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Toaster } from "react-hot-toast";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import { Calendar } from "@/components/ui/calendar";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 
-// Import context
-import { useAuth } from "@/context/authContext";
+// Import context and hooks
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
 import { useUsers } from "@/context/usersContext";
+import { useRouter } from "next/navigation";
+import { useTasks } from "@/context/TasksContext";
 
 // Base schema for all tasks
 const base = z.object({
@@ -57,19 +65,22 @@ const formSchema = z.discriminatedUnion("reoccuring", [
 ]);
 
 export const AddTaskForm = () => {
-  // Access authentication logic
-  const { loading } = useAuth();
-
-  const searchParams = useSearchParams();
-  const presetDate = searchParams.get("date");
-  const presetUserId = searchParams.get("userId");
+  // Access task and users logic
+  const { addTask, loading } = useTasks();
   const { users } = useUsers();
-  const { user } = useAuth();
+
+  // Retrieve URL search parameters from current route
+  const searchParams = useSearchParams();
+  // Extract date from URL if it exists
+  const presetDate = searchParams.get("date");
+  // Extract userId from URL if it exists
+  const presetUserId = searchParams.get("userId");
+
+
+  const router = useRouter();
 
   const [submitted, setSubmitted] = useState(false);
-
-  console.log(users);
-
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // Initialize form
   const form = useForm({
@@ -83,11 +94,58 @@ export const AddTaskForm = () => {
     },
   });
 
+  const reoccuringType = form.watch("reoccuring");
+
   /// /
   // Function to handle submission 
   /// /
-  function onSubmit(values) {
-    console.log(values)
+  async function onSubmit(values) {
+    // Define base task data shared on all submission types
+    const base = {
+      title: values.title,
+      ownerId: values.ownerId,
+    }
+
+    try {
+      // Set submission state to true to disable multiple submits while in progress
+      setSubmitted(true);
+
+      // If task is one-time task
+      if(values.reoccuring === "none") {
+        // Add a single task for specified date
+        await addTask({ ...base, date: values.date })
+      }
+
+      // If task is multiple time task
+      if(values.reoccuring === "multiple") {
+        // Add task for each selected date
+        await Promise.all(
+          values.dateMultiple.map(date => addTask({ ...base, date}))
+        )
+      }
+
+      // If task ranges from two seperate dates
+      if(values.reoccuring === "range") {
+        // Generate all days in selected range
+        const days = eachDayOfInterval({ start: values.dateRange.from, end: values.dateRange.to });
+        // Add task for each day in date range
+        await Promise.all(
+          days.map(date => addTask({ ...base, date}))
+        )
+      }
+
+      // Reset form and redirect user when tasks are added
+      form.reset();
+      router.push("/");
+
+    } catch (error) {
+      // Log error for debugging
+      console.error(error);
+      // Show error message
+      setErrorMessage( "Something went wrong. Please try again later." );
+      // Re-enable submit for user to retry
+      setSubmitted(false);
+    }
   }
 
 
@@ -197,20 +255,95 @@ export const AddTaskForm = () => {
             </FormItem>
           )}
         />
-        {/* ----  DATE ---- */}
+        {/* ---- REOCCURING ---- */}
         <FormField
           control={form.control}
-          name="date"
+          name="reoccuring"
           render={({ field }) => (
             <FormItem>
-                <FormLabel className="text-lg w-1/5">Date</FormLabel>
+              <FormLabel className="text-lg">Reoccurance</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
-                  <Input placeholder="Username" {...field} className="border-2 border-primary focus-visible:ring-secondary focus-visible:border-primary"/>
+                  <SelectTrigger className="w-full justify-between bg-secondary rounded-full hover:bg-secondary-100 border-2 border-primary hover:border-primary-300">
+                    <SelectValue placeholder="Set date reocurrance" />
+                  </SelectTrigger>
                 </FormControl>
+                <SelectContent>
+                  <SelectItem value="none">Single</SelectItem>
+                  <SelectItem value="multiple">Multiple</SelectItem>
+                  <SelectItem value="range">Start to finish</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription className="text-secondary-300">
+                { reoccuringType === "none" && "Pick a single date for the task" }
+                { reoccuringType === "multiple" && "Pick multiple dates for the task to reoccure" }
+                { reoccuringType === "range" && "Pick a start and a finish date for the task" }
+              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+        {/* ----  DATE ---- */}
+        {
+          reoccuringType === "none" && (
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="text-lg w-1/5">Date</FormLabel>
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          className="rounded-md border border-primary"
+                        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )
+        }
+        {
+          reoccuringType === "multiple" && (
+            <FormField
+              control={form.control}
+              name="dateMultiple"
+              render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="text-lg w-1/5">Date</FormLabel>
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          className="rounded-md border border-primary"
+                        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )
+        }
+        {
+          reoccuringType === "range" && (
+            <FormField
+              control={form.control}
+              name="dateRange"
+              render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="text-lg w-1/5">Date</FormLabel>
+                        <Calendar
+                          mode="range"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          className="rounded-md border border-primary"
+                        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )
+        }
         {/* ---- DESCRIPTION ---- */}
         <FormField
           control={form.control}
@@ -225,7 +358,14 @@ export const AddTaskForm = () => {
             </FormItem>
           )}
         />
-      
+        { errorMessage &&
+          <Alert variant="destructive" className="border-2 border-destructive/60">
+              <TbAlertCircleFilled className="h-4 w-4" />
+              <AlertDescription className={cn("text-destructive text-sm font-semibold")}>
+                { errorMessage }
+              </AlertDescription>
+          </Alert>
+        }
         <Button disabled={loading || submitted} type="submit" className="font-bold w-full md:text-sm h-13 md:h-10 md:px-8 md:w-auto">{ loading ? "Creating..." : "Create task" } </Button>
       </form>
     </Form>
